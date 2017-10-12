d4207b573f4a1d756159c59b16d4773323538e88
Evan R <eunoia.github@gmail.com>
2017-10-12 10:51:11 -0700

    handle case when wage is nil, not sure why that would be

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -7,7 +7,7 @@ module Api::V1
 
 		def segments
 			most_recent_date = Score.select("max(created_at) as created_at")[0].created_at.iso8601(6)
-			render json: Score.where(created_at: most_recent_date).where("score <= 0").pluck(:station_id).map{ |sid| Station.find(sid).link.to_a }.map(&:to_h).sort_by{ |s| s["wage"] };
+			render json: Score.where(created_at: most_recent_date).where("score <= 0").pluck(:station_id).map{ |sid| Station.find(sid).link.to_a }.map(&:to_h).sort_by{ |s| s["wage"]||-1 };
 		end
 	end
 end
