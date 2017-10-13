8b3eaeb507a5a19a0bc9adbd9533bd0571703456
Evan R <eunoia.github@gmail.com>
2017-10-13 11:52:26 -0700

    tidy, only look at nyc.

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -7,7 +7,8 @@ module Api::V1
 
 		def segments
 			most_recent_date = Score.select("max(created_at) as created_at")[0].created_at.iso8601(6)
-			render json: Score.where(created_at: most_recent_date).where("score <= 0").pluck(:station_id).map{ |sid| Station.find(sid).link.to_a }.map(&:to_h).sort_by{ |s| s["wage"]||-1 };
+			pick_up_stations = Station.joins(:scores).where("scores.created_at = ? and scores.score <= 0 and stations.region_id=71", most_recent_date).pluck("scores.station_id")
+			render json: pick_up_stations.map{ |sid| Station.find(sid).link.to_a }.map(&:to_h).sort_by{ |s| s["wage"]||-1 };
 		end
 	end
 end
