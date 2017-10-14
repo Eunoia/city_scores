37746416c967257aa55e736c2703217b45610bce
Evan R <eunoia.github@gmail.com>
2017-10-13 21:33:27 -0700

    only show NYC stations on map

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -2,7 +2,7 @@ module Api::V1
 	class RoutesController < ApplicationController
 		def index
 			most_recent_date = Score.select("max(created_at) as created_at")[0].created_at.iso8601(6)
-			render json: Station.joins(:scores).where("scores.created_at = ?", most_recent_date).select("*, scores.score").as_json
+			render json: Station.joins(:scores).where("scores.created_at = ? and stations.region_id=71", most_recent_date).select("*, scores.score").as_json
 		end
 
 		def segments
